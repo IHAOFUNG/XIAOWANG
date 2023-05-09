7e55e167f794d21f1fd0c37e73fe030a51d368c4
IHAOFUNG <133006046+IHAOFUNG@users.noreply.github.com>
2023-05-09 22:17:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oWwrr6TotXruDtA681XRQByEX-gc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'w2DLNq7pb6LCZmB45SCEGsnWpE0pj0xW7-d9KORb0BA',
+      useTemplateId: 'CsIZ0BNADjxx92DvmUUzy1ao606z7KkNTUPc1_4lBAk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-30',
       // 我们在一起已经有xxxx天了的配置
